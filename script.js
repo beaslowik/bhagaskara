459d66fdc465027e4ff33b0ad62d9d5294ef0e6c
BeataSlowik <milvusia@gmail.com>
2015-09-24 10:11:54 +0200

    Changes to be committed: 	modified:   css/style.css 	modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
-$(document).on("ready", function () {
+$(document).on("ready", function() {
 
-    /*
-     ANIMACJA PO KLIKNIECIU W IKONĘ NA JUMBOTRONIE
-     */
-   function scrollViewport() {
-        $(".scrollElementMenu").on("click",
-            function () {
-                var myHref = $(this).data("href");
-                $("html, body").animate({scrollTop: $("#" + myHref).position().top}, 1000);
-            }
-        );
-    }
+      /*
+       MENU SCROLL TO ELEMENT
+       */
+      function scrollViewport() {
+            $(".scrollElementMenu").on("click",
+                  function() {
+                        var myHref = $(this).data("href");
+                        $("html, body").animate({
+                              scrollTop: $("#" + myHref).position().top
+                        }, 1000);
+                  }
+            );
+      }
 
-
-    /*
+      /*
     OUR SKILLS - PROGRESS BAR
    */
-    function setProgressBars() {
-        var howManyBars = $(".box_our_skills_bars_all").length;
-        var arrayOfValues = [];
+      function setProgressBars() {
+            var howManyBars = $(".box_our_skills_bars_all").length;
+            var arrayOfValues = [];
 
-        for (var i = 0; i < howManyBars; i++) {
-            var progressValue = $(".progress_value").eq(i).text();
-            arrayOfValues.push(progressValue);
-        }
-        for(var i = 0; i < howManyBars ; i++){
-            var arrayPercentages = arrayOfValues[i];
-            $(".skills_bar_progress").eq(i).css("width", arrayPercentages+"%");
-        }
-    }
+            for (var i = 0; i < howManyBars; i++) {
+                  var progressValue = $(".progress_value").eq(i).text();
+                  arrayOfValues.push(progressValue);
+            }
+            for (var i = 0; i < howManyBars; i++) {
+                  var arrayPercentages = arrayOfValues[i];
+                  $(".skills_bar_progress").eq(i).css("width", arrayPercentages + "%");
+            }
+      }
 
-    scrollViewport();
-    setProgressBars();
+      scrollViewport();
+      setProgressBars();
 
 });
