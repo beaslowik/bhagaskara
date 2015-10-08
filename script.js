30081c3a94a8c72b20be3da7234c16aa2f391299
BeataSlowik <milvusia@gmail.com>
2015-10-08 20:42:03 +0200

    modified:   css/style.css 	modified:   index.html 	deleted:    index.html.orig 	modified:   script.js sticky menu

--- script.js
+++ script.js
@@ -1,37 +1,67 @@
 $(document).on("ready", function() {
 
-      /*
-       MENU SCROLL TO ELEMENT
-       */
-      function scrollViewport() {
-            $(".scrollElementMenu").on("click",
-                  function() {
-                        var myHref = $(this).data("href");
-                        $("html, body").animate({
-                              scrollTop: $("#" + myHref).position().top
-                        }, 1000);
-                  }
-            );
-      }
+   /*
+    MENU SCROLL TO ELEMENT
+    */
+   function scrollViewport() {
+      $(".scrollElementMenu").on("click",
+         function() {
+            var myHref = $(this).data("href");
+            $("html, body").animate({
+               scrollTop: $("#" + myHref).position().top
+            }, 1000);
+         }
+      );
+   }
 
-      /*
+   /*
     OUR SKILLS - PROGRESS BAR
    */
-      function setProgressBars() {
-            var howManyBars = $(".box_our_skills_bars_all").length;
-            var arrayOfValues = [];
+   function setProgressBars() {
+      var howManyBars = $(".box_our_skills_bars_all").length;
+      var arrayOfValues = [];
 
-            for (var i = 0; i < howManyBars; i++) {
-                  var progressValue = $(".progress_value").eq(i).text();
-                  arrayOfValues.push(progressValue);
-            }
-            for (var i = 0; i < howManyBars; i++) {
-                  var arrayPercentages = arrayOfValues[i];
-                  $(".skills_bar_progress").eq(i).css("width", arrayPercentages + "%");
-            }
+      for (var i = 0; i < howManyBars; i++) {
+         var progressValue = $(".progress_value").eq(i).text();
+         arrayOfValues.push(progressValue);
       }
+      for (var i = 0; i < howManyBars; i++) {
+         var arrayPercentages = arrayOfValues[i];
+         $(".skills_bar_progress").eq(i).css("width", arrayPercentages + "%");
+      }
+   }
+   /*
+   STICKY MENU
+   */
+   function stickyMenu() {
+      console.log($(window.innerWidth));
 
-      scrollViewport();
-      setProgressBars();
+
+      var menu = $("nav");
+      var menuPosition = menu.offset().top;
+      $(window).scroll(function() {
+         if ($(window).scrollTop() > menuPosition) {
+            menu.addClass("sticky");
+         } else if ($(window).scrollTop() < menuPosition) {
+            menu.removeClass("sticky");
+         }
+      })
+   }
+
+   /*
+   PORTFOLIO SELECTION
+   */
+
+
+
+
+
+
+
+
+
+   scrollViewport();
+   setProgressBars();
+   stickyMenu();
 
 });
