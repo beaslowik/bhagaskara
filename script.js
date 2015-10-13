0a3e785193a38fbabc04c035dc79c158ae25a0d0
BeataSlowik <milvusia@gmail.com>
2015-10-13 10:59:00 +0200

    modified:   css/style.css 	modified:   index.html 	modified:   script.js 	Our team - script

--- script.js
+++ script.js
@@ -34,9 +34,6 @@ $(document).on("ready", function() {
    STICKY MENU
    */
    function stickyMenu() {
-      console.log($(window.innerWidth));
-
-
       var menu = $("nav");
       var menuPosition = menu.offset().top;
       $(window).scroll(function() {
@@ -51,14 +48,55 @@ $(document).on("ready", function() {
    /*
    PORTFOLIO SELECTION
    */
+   $("#bt_all").on("click", function() {
+      $(".portfolio_images").removeClass("hide");
+      $(".portfolio_images").addClass("inline_block");
+   });
+   $("#bt_web").on("click", function() {
+      $(".portfolio_images").removeClass("inline_block");
+      $(".portfolio_images").addClass("hide");
+      $("[data-web]").removeClass("hide");
+      $("[data-web]").addClass("inline_block");
+   });
+   $("#bt_apps").on("click", function() {
+      $(".portfolio_images").removeClass("inline_block");
+      $(".portfolio_images").addClass("hide");
+      $("[data-apps]").removeClass("hide");
+      $("[data-apps]").addClass("inline_block");
+   });
+   $("#bt_icons").on("click", function() {
+      $(".portfolio_images").removeClass("inline_block");
+      $(".portfolio_images").addClass("hide");
+      $("[data-icons]").removeClass("hide");
+      $("[data-icons]").addClass("inline_block");
+   });
 
+   /*
+   OUR TEAM
+   */
+   var arrow_left = $("#arrow_left");
+   var arrow_right = $("#arrow_right");
 
-
-
-
-
-
-
+   arrow_right.on("click",
+      function() {
+         var first_shown_person = $(".show_person").first();
+         var right_hidden_person = $(".show_person").last().next(".hide_person");
+         if (right_hidden_person.length == 0) {
+            return 0;
+         }
+         first_shown_person.removeClass("show_person").addClass("hide_person");
+         right_hidden_person.removeClass("hide_person").addClass("show_person");
+      });
+   arrow_left.on("click",
+      function() {
+         var last_shown_person = $(".show_person").last();
+         var left_hidden_person = $(".show_person").first().prev(".hide_person");
+         if (left_hidden_person.length == 0) {
+            return 0;
+         }
+         last_shown_person.removeClass("show_person").addClass("hide_person");
+         left_hidden_person.removeClass("hide_person").addClass("show_person");
+      });
 
    scrollViewport();
    setProgressBars();
